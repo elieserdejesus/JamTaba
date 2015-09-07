4ebf9fe1c3b1a9d76114f414bbdc0c8be5fb736b
Elieser Ademir de Jesus <elieserdejesus@gmail.com>
2015-09-07 20:05:18 -0300

    deploy scripts for windows x86 and x64

--- Client/win64 deploy.bat
+++ Client/win64 deploy.bat
@@ -1,4 +1,4 @@
-@ECHO=OFF
+@echo OFF
 
 SET DEPLOY_DIR=..\deploy-win64
 
@@ -6,25 +6,38 @@ mkdir %DEPLOY_DIR%
 
 SET QT_DIR=C:\Qt-x64\Qt5.5.0\5.5\msvc2013_64\bin
 
-SET SSL_LIBS_DIR=C:\Qt-x64\Qt5.5.0\Tools\QtCreator\bin
+REM run qmake, clean and compile the release version
+
+REM %QT_DIR%\qmake.exe ./Jamtaba2.pro -r -spec win32-msvc2013
+REM %QT_DIR%\..\..\..\Tools\QtCreator\bin\jom.exe clean
+REM %QT_DIR%\..\..\..\Tools\QtCreator\bin\jom.exe -j 4
+REM        %QT_DIR%\..\..\..\Tools\QtCreator\bin\jom.exe -f Makefile.Release
+
+SET SSL_LIBS_DIR=C:\OpenSSL-Win64\bin
 
 SET BUILD_DIR=..\..\build-x64\release
 
-robocopy %BUILD_DIR% %DEPLOY_DIR% Jamtaba2.exe
-robocopy %QT_DIR% %DEPLOY_DIR% Qt5Core.dll
-robocopy %QT_DIR% %DEPLOY_DIR% Qt5Gui.dll
-robocopy %QT_DIR% %DEPLOY_DIR% Qt5Network.dll
-robocopy %QT_DIR% %DEPLOY_DIR% Qt5Widgets.dll
+SET ROBOCOPY_OPTIONS=/NFL /NDL /NJH /NJS /nc /ns /np
 
-robocopy %SSL_LIBS_DIR% %DEPLOY_DIR% ssleay32.dll
-robocopy %SSL_LIBS_DIR% %DEPLOY_DIR% libeay32.dll
+robocopy %BUILD_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% Jamtaba2.exe
+robocopy %QT_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% Qt5Core.dll
+robocopy %QT_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% Qt5Gui.dll
+robocopy %QT_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% Qt5Network.dll
+robocopy %QT_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% Qt5Widgets.dll
 
-robocopy %QT_DIR%\..\plugins\platforms %DEPLOY_DIR%\platforms qwindows.dll
-robocopy %QT_DIR%\..\plugins\bearer %DEPLOY_DIR%\bearer qgenericbearer.dll
-robocopy %QT_DIR%\..\plugins\bearer %DEPLOY_DIR%\bearer qnativewifibearer.dll
+robocopy %SSL_LIBS_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% ssleay32.dll
+robocopy %SSL_LIBS_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% libeay32.dll
 
-robocopy %QT_DIR%\..\plugins\imageformats %DEPLOY_DIR%\imageformats qgif.dll
+robocopy %QT_DIR%\..\plugins\platforms %DEPLOY_DIR%\platforms %ROBOCOPY_OPTIONS% qwindows.dll
+robocopy %QT_DIR%\..\plugins\bearer %DEPLOY_DIR%\bearer %ROBOCOPY_OPTIONS% qgenericbearer.dll
+robocopy %QT_DIR%\..\plugins\bearer %DEPLOY_DIR%\bearer %ROBOCOPY_OPTIONS% qnativewifibearer.dll
 
+robocopy %QT_DIR%\..\plugins\imageformats %DEPLOY_DIR%\imageformats %ROBOCOPY_OPTIONS% qgif.dll
+
+SET MSVC_REDIST_DIR="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x64\Microsoft.VC120.CRT"
+
+robocopy %MSVC_REDIST_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% msvcp120.dll
+robocopy %MSVC_REDIST_DIR% %DEPLOY_DIR% %ROBOCOPY_OPTIONS% msvcr120.dll
 
 
 
