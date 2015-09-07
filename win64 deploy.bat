6746bf03640766f797dc85fe35f74fb519773068
Elieser Ademir de Jesus <elieserdejesus@gmail.com>
2015-09-07 17:54:36 -0300

    removing non used files

--- win64 deploy.bat
+++ win64 deploy.bat
@@ -6,11 +6,9 @@ mkdir %DEPLOY_DIR%
 
 SET QT_DIR=C:\Qt-x64\Qt5.5.0\5.5\msvc2013_64\bin
 
-
 SET SSL_LIBS_DIR=C:\Qt-x64\Qt5.5.0\Tools\QtCreator\bin
 
 SET BUILD_DIR=..\..\build-x64\release
-
 
 robocopy %BUILD_DIR% %DEPLOY_DIR% Jamtaba2.exe
 robocopy %QT_DIR% %DEPLOY_DIR% Qt5Core.dll
@@ -20,8 +18,6 @@ robocopy %QT_DIR% %DEPLOY_DIR% Qt5Widgets.dll
 
 robocopy %SSL_LIBS_DIR% %DEPLOY_DIR% ssleay32.dll
 robocopy %SSL_LIBS_DIR% %DEPLOY_DIR% libeay32.dll
-
-robocopy .\..    %DEPLOY_DIR% GeoLite2-Country.mmdb
 
 robocopy %QT_DIR%\..\plugins\platforms %DEPLOY_DIR%\platforms qwindows.dll
 robocopy %QT_DIR%\..\plugins\bearer %DEPLOY_DIR%\bearer qgenericbearer.dll
