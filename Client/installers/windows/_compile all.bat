8c6b0d7ecaddbd8beacc257d629d621005ee0907
elieserdejesus <elieserdejesus@gmail.com>
2015-10-27 14:46:28 -0200

    removing old scripts

--- Client/installers/windows/_compile all.bat
+++
@@ -1,8 +0,0 @@
-ISCC.exe /Qp "Installer script 32 bits.iss"
-ISCC.exe /Qp "Installer script 64 bits.iss"
-
-rar -inul a "C:\Users\Elieser\Dropbox\Public\jamtaba\jamtaba2\Jamtaba 2 (32 bits) installer.rar" "Jamtaba 2 (32 bits) installer.exe"
-rar -inul a "C:\Users\Elieser\Dropbox\Public\jamtaba\jamtaba2\Jamtaba 2 (64 bits) installer.rar" "Jamtaba 2 (64 bits) installer.exe" 
-
-rm Jamtaba 2 (32 bits) installer.exe
-rm Jamtaba 2 (64 bits) installer.exe
