619e79ef13277ca63538ca90cedfa256e1803f4d
elieserdejesus <elieserdejesus@gmail.com>
2015-11-17 23:46:52 -0200

    applying const correctness in configurator

--- src/configurator.h
+++ src/configurator.h
@@ -23,7 +23,7 @@ private :
     APPTYPE AppType ;//! Contains the type of configuration used : standalone or plugin
     QString HomePath;//! The full path of the root directory in QString format
     QString Pluginpath;//! The full path of the plugin directory in QString format
-    QDir buildDirPaths();//! Used to create the paths of the folder's tree
+    QDir buildDirPaths() ;//! Used to create the paths of the folder's tree
     void createTree();
 
     static void LogHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
@@ -33,6 +33,9 @@ private :
     static QScopedPointer<Configurator> instance;//using a QScopedPointer to auto delete and avoid leak
 
     bool logFileCreated;
+
+    void exportIniFile();
+    void setupIni();
 public:
     static Configurator* getInstance();
     ~Configurator();
@@ -41,19 +44,15 @@ public:
     inline void setFileCreatedFlag(){logFileCreated = true;}
 
     bool setUp(APPTYPE Type);
-    void exportIniFile();
-    void setupIni();
-    QString getIniFilePath();
-    //check if Jamtaba 2 folder exists in application data
-    bool treeExists();
-    bool pluginDirExists();
-    //! Retrieve the root dir
-    QDir getHomeDir() { QDir dir(getHomeDirPath()) ;return dir;}
-    QDir getPluginDir()  ;
-    inline QString getHomeDirPath(){return QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);}
-    inline QString getPluginDirPath(){return getPluginDir().path() ;}
-    inline APPTYPE getAppType(){return AppType; }
-    inline void setAppType(APPTYPE type){AppType=type; }
+    QString getIniFilePath() const;
+    bool treeExists() const;    //check if Jamtaba 2 folder exists in application data
+    bool pluginDirExists() const;
+    inline QDir getHomeDir() const{ return QDir(getHomeDirPath());}
+    QDir getPluginDir() const ;
+    inline QString getHomeDirPath() const {return QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);}
+    inline QString getPluginDirPath() const {return getPluginDir().path() ;}
+    inline APPTYPE getAppType() const{ return AppType; }
+    //inline void setAppType(APPTYPE type){AppType=type; }
 
 };
 #endif // CONFIGURATOR_H
