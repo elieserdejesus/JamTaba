a88d713bd2fbd0434f1308a1ec7fd5facd8983a5
elieserdejesus <elieserdejesus@gmail.com>
2015-12-17 09:26:03 -0200

    refactoring: using Utils to calculte fb value

--- Client/src/gui/BaseTrackView.cpp
+++ Client/src/gui/BaseTrackView.cpp
@@ -101,7 +101,8 @@ void BaseTrackView::updateGuiElements(){
     Audio::AudioPeak peak = mainController->getTrackPeak(getTrackID());
     if(peak.getMax() > maxPeak.getMax()){
         maxPeak.update(peak);
-        double db = 20 * std::log10(maxPeak.getMax());
+
+        double db = Utils::linearToDb(maxPeak.getMax());
         ui->peaksDbLabel->setText(QString::number(db, 'f', 0));
     }
     //update the track peaks
