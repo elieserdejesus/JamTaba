7a7066501bb382f6aef4510cb1ee028171a5c62e
elieserdejesus <elieserdejesus@gmail.com>
2016-04-09 18:13:00 -0300

    Fixing the bug in user name line edit when local tracks are collapsed
     - fixed the bug when collapsing
     - reducing the layout spacing between user name field and collapse button
     - Fixing the bug in "mini mode" local tracks layout initialization
    
     - The old code was setting the 'mini mode' status before the local tracks are created. So the mini mode try iterate in local tracks to reduce this tracks width but there's no tracks at all, these tracks are not created yet. The problem was the use of QTimer to call 'initializeLocalInputChannels()'. Use the QTimer is nice because without this the JamTaba GUI will not open when we have a lot of heavy plugins to load.
    
    - The solution is just put the creation of local tracks and the setting of 'mini' or 'full view' mode inside a function and call this function using QTimer. So, both creation and layout changing are executed in the correct order.

--- src/Common/gui/MainWindow.cpp
+++ src/Common/gui/MainWindow.cpp
@@ -110,7 +110,15 @@ void MainWindow::initialize()
     showBusyDialog(tr("Loading rooms list ..."));
 
     // initialize using last track input settings (using QTimer::singleShot to initialize the tracks (and load the plugins) after Jamtaba is opened).
-    QTimer::singleShot(1, this, SLOT(initializeLocalInputChannels()));
+    QTimer::singleShot(1, this, SLOT(doWindowInitialization()));
+}
+
+void MainWindow::doWindowInitialization()
+{
+    //these two tasks need be executed in sequence because setFullStatus will change the local tracks layout,
+    //and these tracks need be created first.
+
+    initializeLocalInputChannels(); //create the local tracks, load plugins, etc.
 
     // set window mode: mini mode or full view mode
     setFullViewStatus(mainController->getSettings().windowsWasFullViewMode());
@@ -119,12 +127,14 @@ void MainWindow::initialize()
 // ++++++++++++++++++++++++=
 void MainWindow::showPeakMetersOnlyInLocalControls(bool showPeakMetersOnly)
 {
-    foreach (LocalTrackGroupView *channel, localGroupChannels)
+    foreach (LocalTrackGroupView *channel, localGroupChannels) {
         channel->setPeakMeterMode(showPeakMetersOnly);
-    ui.userNameLineEdit->setVisible(!showPeakMetersOnly);
+    }
 
     ui.localControlsCollapseButton->setChecked(showPeakMetersOnly);
     updateLocalInputChannelsGeometry();
+
+    ui.userNameLineEdit->setVisible(!showPeakMetersOnly);
 }
 
 void MainWindow::updateLocalInputChannelsGeometry()
