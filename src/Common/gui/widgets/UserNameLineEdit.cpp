44a41673ad349497de937e51961e7d9b91d682da
elieserdejesus <elieserdejesus@gmail.com>
2016-04-12 21:27:48 -0300

    Replacing spacebar pressing with '_' in user name input

--- src/Common/gui/widgets/UserNameLineEdit.cpp
+++ src/Common/gui/widgets/UserNameLineEdit.cpp
@@ -1,16 +1,37 @@
 #include "UserNameLineEdit.h"
 #include <QValidator>
+#include <QKeyEvent>
 
 UserNameLineEdit::UserNameLineEdit(QWidget *parent)
     :QLineEdit(parent)
 {
-    static QString userNamePattern("[a-zA-Z0-9_-]{1,16}"); //allowing lower and upper case letters, numbers, _ and - symbols, at least 1 character and max 16 characters.
+    static QString userNamePattern("[a-zA-Z0-9 _-]{1,16}"); //allowing lower and upper case letters, numbers, _ and - symbols, blank space, at least 1 character and max 16 characters.
     QRegularExpressionValidator *validator = new QRegularExpressionValidator(QRegularExpression(userNamePattern), this);
     setValidator(validator);
 
     setMaxLength(16); //this maxLength need be changed if we decide support non ASCII user names
 
     connect(this, SIGNAL(editingFinished()), this, SLOT(updateTextAlignment()));
+}
+
+void UserNameLineEdit::keyPressEvent(QKeyEvent *e)
+{
+    QLineEdit::keyPressEvent(getModifiedEvent(e));
+}
+
+void UserNameLineEdit::keyReleaseEvent(QKeyEvent *e)
+{
+    QLineEdit::keyReleaseEvent(getModifiedEvent(e));
+}
+
+QKeyEvent *UserNameLineEdit::getModifiedEvent(QKeyEvent *e)
+{
+    if (e->key() != Qt::Key_Space)
+        return e;
+
+    //replace spacebar with '_'
+    e->accept();
+    return new QKeyEvent(e->type(), Qt::Key_Underscore, e->modifiers(), "_");
 }
 
 void UserNameLineEdit::focusInEvent(QFocusEvent *e)
