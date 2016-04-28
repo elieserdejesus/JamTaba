ba1bb1ceb1884454f16171d1a40e200863698a8d
elieserdejesus <elieserdejesus@gmail.com>
2016-04-28 12:03:58 -0300

    Changing the linux installer to build in 32 bits

--- installers/linux/build_all.sh
+++ installers/linux/build_all.sh
@@ -3,5 +3,9 @@
 #stop the script execution in errors
 set -e
 
-#call installer builder script to compile 64 bits
-sh ./build_installer.sh /usr/local/Qt-5.5.1/bin
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+    sh ./build_installer.sh /usr/local/Qt-5.5.1/bin #compile 64 bits
+else
+    sh ./build_installer.sh /home/elieser/Qt/5.5/gcc/bin
+fi
