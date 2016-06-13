b56840b4f7c1b7f713be4fc7b161b57a890e67dc
elieserdejesus <elieserdejesus@gmail.com>
2016-06-13 08:33:08 -0300

    Fixing compilation error in main project

--- src/VstPlugin/VstPreferencesDialog.cpp
+++ src/VstPlugin/VstPreferencesDialog.cpp
@@ -10,7 +10,7 @@ VstPreferencesDialog::VstPreferencesDialog(QWidget *parent) :
     ui->prefsTab->removeTab(0);
 }
 
-void VstPreferencesDialog::initialize(PreferencesTab initialTab, const Persistence::Settings *settings, const QMap<QString, QString> *jamRecorders)
+void VstPreferencesDialog::initialize(PreferencesTab initialTab, const Persistence::Settings *settings, const QMap<QString, QString> &jamRecorders)
 {
     PreferencesDialog::initialize(initialTab, settings, jamRecorders);
     int tabIndex = initialTab == PreferencesTab::TAB_RECORDING ? 0 : 1;
