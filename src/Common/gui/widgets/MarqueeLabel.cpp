8e247d7e5ecb4e93130c1426055d21ce89c6265f
elieserdejesus <elieserdejesus@gmail.com>
2016-09-05 22:04:02 -0300

    Fixing marquee label copy action behavior
    - Improving MarqueeLabel copy entire text
    - Adding a Marquee manual test

--- src/Common/gui/widgets/MarqueeLabel.cpp
+++ src/Common/gui/widgets/MarqueeLabel.cpp
@@ -7,7 +7,7 @@
 #include <QtMath>
 
 const int MarqueeLabel::MARQUEE_SPEED = 20;// pixels per second
-const int MarqueeLabel::TIME_BETWEEN_ANIMATIONS = 10000;
+int MarqueeLabel::TIME_BETWEEN_ANIMATIONS = 10000;
 
 // +++++++++++++++++++++++++++++++++++++++++++++
 
@@ -15,22 +15,18 @@ MarqueeLabel::MarqueeLabel(QWidget *parent) :
     QLabel(parent),
     textX(0),
     textY(0),
-    textLength(0),
     lastUpdate(0),
     animating(false),
     timeSinceLastAnimation(0),
     speedDecay(1)
 {
-    setAttribute(Qt::WA_NoBackground);
+    //setAttribute(Qt::WA_NoBackground);
     lastUpdate = QDateTime::currentMSecsSinceEpoch();// avoid start a animation when the widget is created
 }
 
-void MarqueeLabel::setText(const QString &newText)
+void MarqueeLabel::setTimeBetweenAnimations(quint32 newTime)
 {
-    // using the elided text when 'newText' is big
-    QLabel::setText(fontMetrics().elidedText(newText, Qt::ElideRight, width()));
-    originalText = newText;
-    reset();
+    MarqueeLabel::TIME_BETWEEN_ANIMATIONS = newTime;
 }
 
 void MarqueeLabel::resetAnimationProperties()
@@ -46,7 +42,6 @@ void MarqueeLabel::reset()
 {
     resetAnimationProperties();
     textY = height() -  (height() - fontInfo().pointSize())/2;
-    textLength = fontMetrics().width(originalText + " ");// adding one blank space to easy read in the animation end
 }
 
 void MarqueeLabel::setFont(const QFont &font)
@@ -57,13 +52,20 @@ void MarqueeLabel::setFont(const QFont &font)
 
 bool MarqueeLabel::needAnimation() const
 {
-    return textLength > width();
+    int textLength = getTextLenght();
+    return textLength > width() && !hasSelectedText();
+}
+
+int MarqueeLabel::getTextLenght() const
+{
+    return fontMetrics().width(text() + " ");
 }
 
 void MarqueeLabel::updateMarquee()
 {
     if (!needAnimation())
         return;
+
     quint64 timeEllapsed = QDateTime::currentMSecsSinceEpoch() - lastUpdate;
     if (!animating) {
         timeSinceLastAnimation += timeEllapsed;
@@ -75,7 +77,7 @@ void MarqueeLabel::updateMarquee()
         int oldTextX = qRound(textX);
         textX -= (timeEllapsed/1000.0f * speedDecay) * MARQUEE_SPEED;
 
-        int totalShiftAmount = textLength - width();// how many pixels text is shifted?
+        int totalShiftAmount = getTextLenght() - width();// how many pixels text is shifted?
         if (totalShiftAmount > 0) {// avoid zero as divider
             float currentShift = qAbs(textX)/totalShiftAmount;
             speedDecay = 1.0f - qPow(currentShift, 3);// exponentially slowing down the animation in the end
@@ -99,18 +101,56 @@ void MarqueeLabel::resizeEvent(QResizeEvent *ev)
 {
     Q_UNUSED(ev);
     reset();
-    setText(originalText);// maybe the new size need elided text
+}
+
+QRectF MarqueeLabel::getSelectedTextRect() const
+{
+    if (!hasSelectedText())
+        return QRectF();
+
+    QFontMetrics metrics = fontMetrics();
+    int textWidth = metrics.width(selectedText());
+    QString firstLetters = text().left(selectionStart());
+    int startPosition = metrics.width(firstLetters);
+
+    return QRectF(startPosition, textY - metrics.ascent(), textWidth, metrics.height());
+}
+
+void MarqueeLabel::mousePressEvent(QMouseEvent *evt)
+{
+    QLabel::mousePressEvent(evt);
+    reset();
 }
 
 void MarqueeLabel::paintEvent(QPaintEvent *evt)
 {
-    if (hasSelectedText()) {
+    QString elidedText = fontMetrics().elidedText(text(), Qt::ElideRight, width());
+    bool needEliding = elidedText.size() != text().size();
+    if (!animating && !needEliding) {
         QLabel::paintEvent(evt);
     }
     else {
         QPainter p(this);
         p.setFont(font());
-        p.setBrush(palette().text());// use the color defined in css file
-        p.drawText(QPointF(textX, textY), animating ? originalText : text());  // text() can be elided
+
+        qreal x = textX;
+        qreal y = textY;
+        QRectF selectedRect = getSelectedTextRect();
+        if (!animating) {//not animating and need eliding
+            x = 0; // when eliding text is always left aligned
+
+            if (hasSelectedText()) {
+                p.fillRect(selectedRect, palette().highlight()); //draw the
+            }
+        }
+
+        p.setPen(QPen(palette().text(), 1.0)); // using the color defined in css file
+        p.drawText(QPointF(x, y), !animating ? elidedText : text()); //draw the entire text
+
+        if (hasSelectedText()) { //draw the selected text with highlight color
+            p.setPen(QPen(palette().highlightedText(), 1.0));
+            p.drawText(QPointF(selectedRect.left(), textY), selectedText());
+        }
     }
 }
+
