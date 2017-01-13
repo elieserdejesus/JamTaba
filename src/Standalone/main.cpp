d377dddad407fa2ba2cbd427885406508608bc8b
elieserdejesus <elieserdejesus@gmail.com>
2017-01-13 12:02:45 -0200

    No more problem in main

--- src/Standalone/main.cpp
+++ src/Standalone/main.cpp
@@ -37,12 +37,8 @@ int main(int argc, char* args[] ){
     MainWindowStandalone mainWindow(&mainController);
     mainController.setMainWindow(&mainWindow);
 
-
-    //if these 2 line order is changed the application freeze in Mac OSX :(
-
-    mainWindow.show();
     mainWindow.initialize();
-
+    mainWindow.show();
 
     mainController.connectInJamtabaServer();
 
