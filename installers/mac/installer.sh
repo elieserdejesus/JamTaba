3da493b288c9ce718a23212c9891b3bf19ee5d83
elieserdejesus <elieserdejesus@gmail.com>
2017-01-15 19:40:51 -0200

    Fixing Mac installer

--- installers/mac/installer.sh
+++ installers/mac/installer.sh
@@ -1,30 +1,30 @@
 set -e
 
+echo "Compiling AU plugin ..."
+#compiling the AU plugin (this is a Xcode project, not a Qmake project)
+#xcodebuild -project ../../PROJECTS/AUPlugin/JamTaba.xcodeproj -scheme JamTaba clean build
+
+echo "AU plugin compiled!"
+
+#IMPORTANT: the AUPlugin project is running a post build script and copying JamTaba.component to /Library/Audio/Plug-Ins/Components
+
+
+echo "Compiling Jamtaba Standalone, VstScanner and AUScanner..."
+
 mkdir -p ~/Desktop/Release
 
 cd ~/Desktop/Release
 
 echo “Compiling …”
 
-#~/Desktop/Qt-5.6/bin/qmake -config release ~/Desktop/JamTaba/PROJECTS/Jamtaba.pro -r -spec macx-clang CONFIG+=x86_64
+~/Desktop/Qt-5.6/bin/qmake -config release ~/Desktop/JamTaba/PROJECTS/Jamtaba.pro -r -spec macx-clang CONFIG+=x86_64
 
 #compile the translations file
-#~/Desktop/Qt-5.6/bin/lrelease ~/Desktop/JamTaba/PROJECTS/Jamtaba.pro
+~/Desktop/Qt-5.6/bin/lrelease ~/Desktop/JamTaba/PROJECTS/Jamtaba.pro
 
 
-#/usr/bin/make -s -j 4
+/usr/bin/make -s -j 4
 
-
-#~/Desktop/Qt-5.6/bin/macdeployqt ~/Desktop/Release/Standalone/Jamtaba2.app -verbose=2
-
-#copy the VstScanner inside Jamtaba2.app
-cp ~/Desktop/Release/VstScanner/VstScanner ~/Desktop/Release/Standalone/Jamtaba2.app/Contents/MacOS/	
-
-
-#compiling the AU plugin (this is a Xcode project, not a Qmake project)
-#xcodebuild -parallelizeTargets -jobs 4 -target JamTaba -project ~/Desktop/JamTaba/PROJECTS/AUPlugin/JamTaba.xcodeproj build
-
-#IMPORTANT: the AUPlugin project is running a post build script and copying JamTaba.component to /Library/Audio/Plug-Ins/Components
 
 
 # node-appdmg search the files considering .json relative paths. Putting the .app file in the node-appdmg .json folder
