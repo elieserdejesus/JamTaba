254cc6cb644ffe8d0687ed8ef2ad1c9c625e62e6
jonjamcam <jonjamcam@gmail.com>
2017-04-08 07:51:38 -0300

    jp_JP, fr_FR and de_DE missing transaltions update (#810)
    * jp_fr_de missing transaltion update
    
    * Update fr_FR.ts

--- translations/fr_FR.ts
+++ translations/fr_FR.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<TS version="2.0" language="fr_FR">
 <context>
     <name>Audio::Looper</name>
     <message>
@@ -447,7 +447,7 @@
     </message>
     <message>
         <source>Loop file already exists. Do you want to overwrite?</source>
-        <translation type="unfinished"></translation>
+        <translation>Le fichier Loop existe déjà. Voulez-vous remplacer?</translation>
     </message>
 </context>
 <context>
