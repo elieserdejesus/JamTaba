731fc3631deca73835c16c747fe3f58a4f2c12fc
elieserdejesus <elieserdejesus@gmail.com>
2017-09-15 08:14:18 -0300

    Changing version to '2.1.0'

--- src/Common/Configurator.h
+++ src/Common/Configurator.h
@@ -6,7 +6,7 @@
 #include <QScopedPointer>
 
 // Define the preprocessor macro to get the app version in Jamtaba.
-#define VERSION "2.0.21"
+#define VERSION "2.1.0"
 #define APP_VERSION VERSION
 
 // ! Configurator class for Jamtaba !
