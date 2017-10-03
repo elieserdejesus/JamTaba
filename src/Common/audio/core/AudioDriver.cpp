dab6905a9bfb2ff8bd6597d7502f36b752ba227b
elieserdejesus <elieserdejesus@gmail.com>
2017-10-02 22:34:08 -0300

    :art: Allocating audio driver I/O buffer in stack

--- src/Common/audio/core/AudioDriver.cpp
+++ src/Common/audio/core/AudioDriver.cpp
@@ -46,8 +46,8 @@ AudioDriver::AudioDriver(Controller::MainController *mainController) :
     audioDeviceIndex(0),
     sampleRate(44100),
     bufferSize(128),
-    inputBuffer(nullptr),
-    outputBuffer(nullptr),
+    inputBuffer(SamplesBuffer(2)),
+    outputBuffer(SamplesBuffer(2)),
     mainController(mainController)
 {
 
@@ -55,9 +55,16 @@ AudioDriver::AudioDriver(Controller::MainController *mainController) :
 
 void AudioDriver::recreateBuffers()
 {
-    inputBuffer.reset(new SamplesBuffer(globalInputRange.getChannels()));
+    if (globalInputRange.isMono())
+        inputBuffer.setToMono();
+    else
+        inputBuffer.setToStereo();
 
-    outputBuffer.reset(new SamplesBuffer(globalOutputRange.getChannels()));
+
+    if (globalOutputRange.isMono())
+        outputBuffer.setToMono();
+    else
+        outputBuffer.setToStereo();
 }
 
 AudioDriver::~AudioDriver()
