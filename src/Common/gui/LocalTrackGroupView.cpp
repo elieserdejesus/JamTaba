5c3b008ef84979281a154f255dfc5afe44155d8b
jonjamcam <jonjamcam@users.noreply.github.com>
2018-04-29 03:45:57 -0300

    Showing video icon

--- src/Common/gui/LocalTrackGroupView.cpp
+++ src/Common/gui/LocalTrackGroupView.cpp
@@ -53,7 +53,6 @@ void LocalTrackGroupView::setAsVideoChannel()
     setInstrumentIcon(instrumentIcon);
     instrumentsButton->setStyleSheet(QString("margin-left: 0px"));
     instrumentsButton->blockSignals(true);
-    instrumentsButton->setVisible(false);
     xmitButton->setVisible(false);
 
     auto tracks = getTracks<LocalTrackView *>();
