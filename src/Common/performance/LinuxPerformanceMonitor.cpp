c569856e657b5841f033c777dcd34c6b02dcc44b
arnaudlecam <arnaudlecam@yahoo.fr>
2020-04-07 02:39:35 +0200

    getMemmoryUsed coded for Linux + memmory thresold inverted

--- src/Common/performance/LinuxPerformanceMonitor.cpp
+++ src/Common/performance/LinuxPerformanceMonitor.cpp
@@ -1,5 +1,6 @@
 #include "PerformanceMonitor.h"
 
+#include "sys/sysinfo.h"
 
 PerformanceMonitor::PerformanceMonitor(){
 
@@ -10,8 +11,10 @@ PerformanceMonitor::~PerformanceMonitor(){
 }
 
 int PerformanceMonitor::getMemmoryUsed(){
-
-    return 0;
+    struct sysinfo memInfo;
+    sysinfo (&memInfo);
+    long long virtualMemUsed = memInfo.totalram + memInfo.totalswap + memInfo.totalhigh - memInfo.freeram - memInfo.freeswap - memInfo.freehigh;
+    return 100 * virtualMemUsed / (memInfo.totalram + memInfo.freeswap + memInfo.freehigh);
 }
 
 int PerformanceMonitor::getBatteryUsed()
