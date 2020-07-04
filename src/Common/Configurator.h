116a36aed2ea611380ade3e541d3806ec3502413
Nick Donaldson <ndonald2@gmail.com>
2020-07-04 17:21:50 -0600

    Bump beta version num

--- src/Common/Configurator.h
+++ src/Common/Configurator.h
@@ -6,7 +6,7 @@
 #include <QScopedPointer>
 
 // Define the preprocessor macro to get the app version in Jamtaba.
-const QString VERSION = "2.2-sync-beta-1";
+const QString VERSION = "2.2.0-sync-beta-2";
 #define APP_VERSION VERSION
 
 // ! Configurator class for Jamtaba !
